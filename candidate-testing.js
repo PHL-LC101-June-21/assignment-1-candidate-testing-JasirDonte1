acc7fec462b6459f52db87f0b588dc811652fa06
Jasir Massey-Campbell <33403498+JasirDonte1@users.noreply.github.com>
2021-06-14 22:31:37 +0000

    Formatted the print statements for Part 3

--- candidate-testing.js
+++ candidate-testing.js
@@ -8,7 +8,7 @@ let candidateName = "";
 let question = ["Who was the first American woman in space? "];
 let correctAnswer = "Sally Ride";
 let candidateAnswer = "";
-let questions = ["Who was the first American woman in space?	","True or false: 5 kilometer == 5000 meters?", "(5 + 3)/2 * 10 = ?", "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2?", "What is the minimum crew size for the ISS?"];
+let questions = ["Who was the first American woman in space?	","True or false: 5 kilometer == 5000 meters? ", "(5 + 3)/2 * 10 = ? ", "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", "What is the minimum crew size for the ISS? "];
 let correctAnswers = ["Sally Ride","true","40","Trajectory","3"];
 let candidateAnswers = [];
 
@@ -24,7 +24,8 @@ function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
   //candidateAnswer = input.question(question);
   for (i = 0; i < questions.length; i++){
-    candidateAnswers[i] = input.question(questions[i]+"\nanswer: ");
+    candidateAnswers[i] = input.question((i+1)+") "+questions[i]+"\nYour Answer: ");
+    console.log("Correct Answer: "+correctAnswers[i]+"\n");
   }
 
 }
@@ -40,7 +41,6 @@ function gradeQuiz(candidateAnswers) {
     score++;
   } */
   for (i = 0; i < questions.length; i++){
-    console.log("your answer: " + candidateAnswers[i] + "  | correct answer: " + correctAnswers[i]);
     if(candidateAnswers[i] === correctAnswers[i]){
       score++;
     }
@@ -48,7 +48,12 @@ function gradeQuiz(candidateAnswers) {
 
   let grade = 100*(score / questionsCount);
 
-  console.log("score: " + score + "/" + questionsCount + "  (", grade, "%)");
+  console.log("Overall Grade: "+grade+"% ("+score+" of "+questionsCount+" responses correct)");
+  if(grade >= 80){
+    console.log("Status: PASSED");
+  }else{
+    console.log("Status: FAILED");
+  }
 
   return grade;
 }
